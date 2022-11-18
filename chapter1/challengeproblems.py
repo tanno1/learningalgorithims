e408028682e134968d40018f88b6d197822c4ee3
tanno <94481731+tanno1@users.noreply.github.com>
2022-11-18 10:48:14 -0800

    chapter 1 p1

--- chapter1/challengeproblems.py
+++ chapter1/challengeproblems.py
@@ -1,3 +1,27 @@
 #Noah Tanner
 #Chapter 1 Challenge Problems
 #November 2022
+
+#Palindrome Word Detector, slice method
+
+def slice_palindrome(n):
+    if n[::-1] == n:
+        print('{} is a palindrome'.format(n))
+    else: 
+        print('{} is not a palindrome'.format(n))
+
+#Test
+print('first test:')
+n = 'madam'
+results_1 = slice_palindrome(n)
+print(results_1)
+
+print('second test:')
+n = 'tacocat'
+results_2 = slice_palindrome(n)
+print(results_2)
+
+print('third test:')
+n = 'venezuela'
+results_3 = slice_palindrome(n)
+print(results_3)
